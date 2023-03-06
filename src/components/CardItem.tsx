57278a222a25d81412379110da0826c4c8c1d145
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-03-06 23:05:10 +0300

    add: card item

---
+++ src/components/CardItem.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+type IProps = {
+  question: string;
+  answer: string;
+};
+
+const CardItem: FC<IProps> = ({ question, answer }) => {
+  return (
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography color="primary.light">{question}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography>{answer}</Typography>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
+
+export default CardItem;
